9f17f227e56a7de7fb0b4fada4d70fc041b36f7a
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-11-01 08:45:02 -0300

    feat: setting cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 
 const prisma = new PrismaClient({ log: ["query"] });
 
 async function start() {
   const fastify = Fastify({ logger: true });
+  await fastify.register(cors, { origin: true }); // trocar o valor de origin pelo domínio do frontend
 
   fastify.get("/pools/count", async () => {
     const count = await prisma.pool.count();
@@ -12,7 +14,7 @@ async function start() {
     return { count };
   });
 
-  await fastify.listen({ port: 3333 });
+  await fastify.listen({ port: 3333, host: "0.0.0.0" }); // configurar host para funcionar no android
 }
 
 start();
