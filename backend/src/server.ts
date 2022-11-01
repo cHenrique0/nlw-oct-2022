48ee653bd1e26873c127e96b0b24dcbc1c4d9c5c
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-11-01 08:22:46 -0300

    feat: inserting database with prisma orm

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,16 @@
+import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
+
+const prisma = new PrismaClient({ log: ["query"] });
 
 async function start() {
   const fastify = Fastify({ logger: true });
+
+  fastify.get("/pools/count", async () => {
+    const count = await prisma.pool.count();
+
+    return { count };
+  });
 
   await fastify.listen({ port: 3333 });
 }
