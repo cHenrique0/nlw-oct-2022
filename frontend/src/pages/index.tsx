4a02693e9c088cb5bee81796208c85937a64bb8c
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-11-02 20:43:02 -0300

    feat: adding landing page HTML structure

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,17 +1,68 @@
-interface HomeProps {
-  count: number;
-}
+import Image from "next/image";
 
-export default function Home(props: HomeProps) {
+// Images
+import mobileAppPreviewImg from "../assets/mobile-preview-nlw-copa.png";
+import logoImg from "../assets/logo.svg";
+import avatarExampleImg from "../assets/avatares.png";
+import iconCheckImg from "../assets/icon-check.svg";
+
+/* interface HomeProps {
+  count: number;
+} */
+
+export default function Home() {
   return (
-    <h1 className="text-violet-500 font-bold text-4xl">
-      Contagem: {props.count}
-    </h1>
+    <div>
+      <main>
+        <Image src={logoImg} alt="NLW Copa" />
+
+        <h1>Crie seu bolão e compartilhe entre amigos</h1>
+
+        <div>
+          <Image src={avatarExampleImg} alt="" />
+          <strong>
+            <span>+12.592</span> pessoas já estão usando
+          </strong>
+        </div>
+
+        <form>
+          <input type="text" placeholder="Qual o nome do seu bolão?" />
+          <button type="submit">Criar meu bolão</button>
+        </form>
+
+        <p>
+          Após criar seu bolão, você receberá um código único que poderá usar
+          para convidar outras pessoas.
+        </p>
+
+        <div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+              <span>+2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+              <span>+10.493</span>
+              <span>Palpites enviados</span>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Image
+        src={mobileAppPreviewImg}
+        alt="Dois celulares exibindo uma prévia da aplicação móvel no NLW Copa"
+        quality={100}
+      />
+    </div>
   );
 }
 
 // Backend (API) request
-export const getServerSideProps = async () => {
+/* export const getServerSideProps = async () => {
   const response = await fetch("http://localhost:3333/pools/count");
   const data = await response.json();
 
@@ -20,4 +71,4 @@ export const getServerSideProps = async () => {
       count: data.count,
     },
   };
-};
+}; */
