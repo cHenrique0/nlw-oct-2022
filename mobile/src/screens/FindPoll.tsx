ad560316c0a6375689b382944f81eb5acc306c87
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-11-05 18:00:00 -0300

    refactor: from pool to poll

--- mobile/src/screens/FindPoll.tsx
+++ mobile/src/screens/FindPoll.tsx
@@ -4,7 +4,7 @@ import { Header } from "../components/Header";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
-export function FindPool() {
+export function FindPoll() {
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Buscar por código" showBackButton />
